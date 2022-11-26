0a1c53faca5c8b24a145212073cd04d93df76ef4
xielina <943714607@qq.com>
2022-11-26 22:03:37 +0800

    首次添加

---
+++ src/test/java/com/xt/gradle01/Gradle01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xt.gradle01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gradle01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
