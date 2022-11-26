06a36ba9b6ea42df2d51725a533b8f7b7e03a6d8
xielina <943714607@qq.com>
2022-11-26 23:15:35 +0800

    push commit 2

--- src/main/java/com/xt/gradle01/Gradle01Application.java
+++ src/main/java/com/xt/gradle01/Gradle01Application.java
@@ -15,6 +15,7 @@ public class Gradle01Application {
 		System.out.println("hot-fix test");
 		System.out.println("master test");
 		System.out.println("push test");
+		System.out.println("push test 2");
 	}
 
 }
